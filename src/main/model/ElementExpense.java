224905267e34bc9f1e7a6cd149dd49fe5dea9096
Ramnik <ramniksidhu2k03@gmail.com>
2024-10-08 04:56:10 -0700

    Added Specification for ElementExpense class

---
+++ src/main/model/ElementExpense.java
@@ -0,0 +1,38 @@
+package model;
+
+public class ElementExpense {
+    private int expense;
+    private String category;
+    private String date;
+
+    //EFFECTS: constructs a new element expense with a date and a category.
+    public ElementExpense(int expense, String category, String date) {
+        //stub
+    }
+
+    public int getExpense() {
+        return 0;
+    }
+    public String getDate() {
+        return null;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    // public void setExpense() {
+    //     this.expense=expense;
+    // }
+   
+
+    // public void setCategory() {
+    //     //stub
+    // }
+
+    // public void setDate() {
+    //     //stub
+    // }
+
+
+}
