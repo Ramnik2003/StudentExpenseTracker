acf1db3ba5a75bb6f4e30478d17ef4588b4e9a4e
Ramnik <ramniksidhu2k03@gmail.com>
2024-10-10 23:39:07 -0700

    Implemented Main() class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,6 +2,8 @@ package ui;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to my project!");
+        System.out.println("Welcome to Student Expense Tracker");
+        new ExpenseTrackerApp();
+        
     }
 }
