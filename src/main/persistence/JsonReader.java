d6f9f19d98a01477f6bcbb1e9e702577e45a2dbe
Ramnik <ramniksidhu2k03@gmail.com>
2024-10-22 23:32:35 -0700

    Added method specifications to persistence

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -4,16 +4,19 @@ import model.ElementExpense;
 import model.ExpensesOperations;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.io.IOException;
 
-//Represents a reader that reads workroom from JSON data stored in file
+//Represents a reader that reads ExpenseOperations from JSON data stored in file
 public class JsonReader {
-    private String source;
 
     //EFFECTS: constructs reader to read from the source file
     public JsonReader(String source) {
-        this.source = source;
+        //stub
     }
 
-    //
+    //EFFECTS: reads ExpenseOperations from file and returns it
+    public ExpensesOperations read() throws IOException {
+        return null;
+    }
 
 }
--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -3,10 +3,36 @@ package persistence;
 import model.ElementExpense;
 import model.ExpensesOperations;
 import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+
 import org.json.JSONArray;
 
-
+//Represents a writer that writes JSON representation of ExpenceOperation to file
 public class JsonWriter {
 
+    //EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        //stub
+    }
+
+    //REQUIRES: destination file should be open for writing
+    //MODIFIES: this
+    //EFFECTS: opens writer 
+    public void open() throws FileNotFoundException {
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: writes JSON representation of ExpenseOpertions to file 
+    public void write(ExpensesOperations et) {
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: closes the writer
+    public void close() {
+        //stub
+    }
 
 }
