8c712fa68e09308b0c9c242ba8b844dbfea3d4f0
Ramnik <ramniksidhu2k03@gmail.com>
2024-10-24 12:19:11 -0700

    Bug corrected - Writable class

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -3,8 +3,9 @@ package persistence;
 import org.json.JSONObject;
 
 
-public class Writable {
+public interface Writable {
+
     //EFFECTS: returns this as JSON object
     JSONObject toJson();
-
-}
+    
+} 
