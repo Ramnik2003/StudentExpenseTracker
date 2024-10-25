3ddf620e24f5eb45f99fbd0953eb0a6d27156c72
Ramnik <ramniksidhu2k03@gmail.com>
2024-10-25 11:48:12 -0700

    Implemented JsonReader and JsonWriter classes

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -5,18 +5,67 @@ import model.ExpensesOperations;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.stream.Stream;
 
 //Represents a reader that reads ExpenseOperations from JSON data stored in file
 public class JsonReader {
+    private String source;
+    private String jsonData;
 
     //EFFECTS: constructs reader to read from the source file
     public JsonReader(String source) {
-        //stub
+        this.source = source;
     }
 
     //EFFECTS: reads ExpenseOperations from file and returns it
     public ExpensesOperations read() throws IOException {
-        return null;
+        jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseExpensesOperations(jsonObject);
+
+    }
+
+    //EFFECTS: reads the source file and returns it 
+    private String readFile(String source) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
+
+    }
+
+    // EFFECTS: parses ExpenseOpereations from JSON object and returns it
+    private ExpensesOperations parseExpensesOperations(JSONObject jsonObject) {
+        ExpensesOperations expenses = new ExpensesOperations();
+        addExpenses(expenses, jsonObject);
+        return expenses;
+    }
+
+    // MODIFIES: expenses
+    // EFFECTS: parses expenses from JSON object and adds them to ExpenseOpereations
+    private void addExpenses(ExpensesOperations expenses, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("expenses");
+        for (Object json : jsonArray) {
+            JSONObject nextExpense = (JSONObject) json;
+            addExpense(expenses, nextExpense);
+        }
+    }
+
+    // MODIFIES: expenses
+    // EFFECTS: parses expense from JSON object and adds it to ExpensesOperations
+    private void addExpense(ExpensesOperations expenses, JSONObject jsonObject) {
+        double amount = jsonObject.getDouble("amount");
+        String category = jsonObject.getString("category");
+        LocalDate date = LocalDate.parse(jsonObject.getString("date"));      
+        ElementExpense expense = new ElementExpense(amount, category, date);
+        expenses.addExpense(expense);
     }
 
 }
--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -4,7 +4,8 @@ import model.ElementExpense;
 import model.ExpensesOperations;
 import org.json.JSONObject;
 
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -23,19 +24,19 @@ public class JsonWriter {
     //REQUIRES: destination file should be open for writing
     //MODIFIES: this
     //EFFECTS: opens writer 
-    public void open() throws IOException {
-        writer = new PrintWriter(new FileWriter(destination));
+    public void open() throws FileNotFoundException {
+        writer = new PrintWriter(new File(destination));
     }
 
     //MODIFIES: this
     //EFFECTS: writes JSON representation of ExpenseOpertions to file 
     public void write(ExpensesOperations et) {
         JSONObject jsonObject = new JSONObject();
-
         JSONArray jsonExpenses = new JSONArray();
+        
         for (ElementExpense expense : et.getExpensesList()) {
             JSONObject jsonExpense = new JSONObject();
-            jsonExpense.put("name", expense.getCategory());
+            jsonExpense.put("date", expense.getDate().toString());
             jsonExpense.put("amount", expense.getExpense());
             jsonExpense.put("category", expense.getCategory());
             jsonExpenses.put(jsonExpense);
