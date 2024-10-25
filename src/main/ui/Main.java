7ee6ab315d55ff9849fc1b3edb7f0e58347bcd65
Ramnik <ramniksidhu2k03@gmail.com>
2024-10-25 11:48:49 -0700

    Modified Main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,9 +1,18 @@
 package ui;
 
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Student Expense Tracker");
-        new ExpenseTrackerApp();
+        // System.out.println("Welcome to Student Expense Tracker");
+        // new ExpenseTrackerApp();
+        try {
+            new ExpenseTrackerApp();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to run application: file not found");
+        }
         
     }
+
+
 }
