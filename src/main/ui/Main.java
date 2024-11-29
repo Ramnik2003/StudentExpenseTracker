14e4b51922d4eb764691a640eebca2ef5d70ca6d
Ramnik <ramniksidhu2k03@gmail.com>
2024-11-29 12:51:17 -0800

    Bug fixed

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,11 +4,15 @@ import javax.swing.SwingUtilities;
 
 //REFERENCE: I refered to Youtube chanel "Java Code Junkie" to learn about GUI 
 // https://www.youtube.com/watch?v=1vVJPzVzaK8&list=PL3bGLnkkGnuV699lP_f9DvxyK5lMFpq6U 
+// https://stackoverflow.com/questions/22145515/what-is-swingutilities-invokelaternew-runnable
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new ExpenseTrackerGUI().setVisible(true));
-        
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run() {
+                new ExpenseTrackerGUI();
+            }});
     }
 
 
